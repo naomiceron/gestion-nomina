ac15f64e2ef3c689de750000c15141f794d4ab20
naomiceron <58761246+naomiceron@users.noreply.github.com>
2021-12-14 20:38:07 -0700

    test6

--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
 require("dotenv").config();
 
 const { Pool } = require("pg");
-const isProduction = process.env.NODE_ENV === "production";
-
-const connectionString = `postgres://twqcwhuvbjpvep:68aac0bf92f7cd8936df8ca4b763d7bcb37bfdec4ef3ecea6235b8c28e540812@ec2-44-193-111-218.compute-1.amazonaws.com:5432/dtvn1svfmp0ts`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = { pool };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-const { Pool } = require("pg");
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
 require("./config.js");
 const app = require("./app");
