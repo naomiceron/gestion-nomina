3d151f44c528dd48a396a5e70bb305e0d31098b5
unknown <andreazayasb@gmail.com>
2021-12-14 18:59:49 -0700

    intento api 1

--- src/app.js
+++ src/app.js
@@ -1,26 +1,56 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const morgan = require("morgan");
 const cors = require("cors");
-
-/*
-const peopleRoutes = require('./routes/people.routes')
-const authRoutes = require('./routes/auth.routes')
-const feedbackRoutes = require('./routes/feedback.routes')
-const teamsRoutes = require('./routes/teams.routes')
-const radsRoutes = require('./routes/rads.routes') 
-*/
+const { pool } = require('./config');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
+app.use(              //?
   cors({
     origin: "*",
   })
 );
 
-app.use(morgan("dev"));
+app.use(morgan("dev")); //?
 app.use(express.json());
 
-console.log("success");
+const getSolicitud = (request, response) => {
+  pool.query('SELECT * FROM SOLICITUDNOM', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const addRevision = (request, response) => {
+  const { fechaRevision, estadoRevision } = request.body
+
+  pool.query(
+    'INSERT INTO SOLICITUDNOM (fechaRevision, estadoRevision) VALUES (2021-12-01, Aprobado)',
+    [fechaRevision, estadoRevision],
+    (error) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({ status: 'success', message: 'Solicitud Revisada.' })
+    }
+  )
+}
+
+app
+  .route('/Solicitud')
+  // GET endpoint
+  .get(getSolicitud)
+  // POST endpoint
+  .post(addRevision)
+
+// Start server
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`)
+})
 
 module.exports = app;
